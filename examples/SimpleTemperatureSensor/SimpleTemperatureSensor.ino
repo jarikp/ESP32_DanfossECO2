d4e326876153e62f987c2335bbe5a3380223185c
Jarik Poplavski <jarik@poplavski.net>
2020-05-08 12:52:32 +0200

    added some usablity comments to the example

--- examples/SimpleTemperatureSensor/SimpleTemperatureSensor.ino
+++ examples/SimpleTemperatureSensor/SimpleTemperatureSensor.ino
@@ -1,6 +1,25 @@
+/*
+  SimpleTemperatureSensor
+  
+  An example of how to use Danfoss ECO 2 eTRV as simple room temperature sensor.
+
+  For documentation visit: https://github.com/jarikp/ESP32_DanfossECO2
+  
+  Copyright (c) 2020 Jarik Poplavski 
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
 #include <Arduino.h>
 #include <DanfossECO2.h>
 
+// PLEASE PROVIDE CORRECT KEY DATA 
 const uint8_t secretKey[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 DanfossECO2 *eTRV;
@@ -11,7 +30,10 @@ void setup()
 
     BLEDevice::init("");
 
+    // PLEASE PROVIDE VALID MAC ADDRESS OF YOUR eTRV 
     eTRV = new DanfossECO2("00:00:00:00:00:00");
+
+    // PLEASE PROVIDE CORRECT PIN CODE (IF IT'S SET)
     eTRV->usePin(0000);
 
     Serial.println("Starting DanfossECO2 temperature monitor loop, reading data every 10 second...");
